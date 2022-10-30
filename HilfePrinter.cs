74d72025dc139df0bbeee8e36b497a8d4b7a71fe
sturnz <jar.bielefeld@gmail.com>
2022-10-30 20:06:32 +0100

    typo

--- HilfePrinter.cs
+++ HilfePrinter.cs
@@ -12,13 +12,13 @@ namespace Jasmin1
         {
             Console.WriteLine(  "Provisionsrechner Version 1.0\n\n"                     +
 
-                                "[Optional arguments]\n\n"                                +
+                                "[Optional arguments]\n\n"                              +
 
                                 "-dir       Arbeitsverzeichnis\n" +
                                 "-pa        Provisionsanteil in Prozent (Ganzzahl)\n"   +
                                 "-dak       Name der Datenbank für Kundendaten\n"       +
                                 "-dav       Name der Datenbank für Vertragsdaten\n"     +
-                                "-ft        Filetype der Datenbanken (.csv, .txt)\n"  +
+                                "-ft        Filetype der Datenbanken (.csv, .txt)\n"    +
                                 "-h         Help\n\n"                                   +
 
                                 "Für nicht eingegebene Argumente werden die Default Einstellungen aus App.config verwendet.");
